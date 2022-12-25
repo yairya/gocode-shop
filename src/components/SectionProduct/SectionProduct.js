98477414d118e51d9d3df9987f164ed5ae355c1f
yairy <yairy6@gmail.com>
2022-12-25 22:57:40 +0200

    take fake product from https://fakestoreapi.com/products

--- src/components/SectionProduct/SectionProduct.js
+++ src/components/SectionProduct/SectionProduct.js
@@ -6,7 +6,7 @@ import ProductCard from "../ProductCard/ProductCard"
 const SectionProduct = ({products}) => {
     return(
         <section className="products">
-            {products.map( (product, index) => <ProductCard key={index} imgSrc={product.imgSrc} title={product.title} price={product.price} />)}
+            {products.map( (product, index) => <ProductCard key={index} imgSrc={product.image} title={product.title} price={product.price} />)}
         </section>
     )
 }
