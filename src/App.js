8a4897705f1e49204033b0d1e7a7504a438faf9e
yairy <yairy6@gmail.com>
2023-01-01 18:45:53 +0200

    remove first page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React ,{useState} from 'react'
 import './App.css';
+import Nav from "./components/Nav/Nav"
+import SectionProduct from "./components/SectionProduct/SectionProduct"
 import appData from "./data/AppData"
-import FirstPage from "./components/FirstPage/FirstPage"
 
 function App() {
+  const [currentCategory, setCategory] = useState("All categories");
+  const setCurrentCategory = (category) =>
+  {
+    setCategory(category);
+  }    
+  appData.useState.currentCategory.setCategory = setCurrentCategory;
+
+  appData.categories = appData.products.map(p => p.category).filter((value, index, array) => array.indexOf(value)===index);
   return (
-    <div className="App">
-      <FirstPage appData={appData}/>
+    <div>
+        <Nav appData={appData}/>
+        <SectionProduct products={appData.products} category={currentCategory}/>
     </div>
   );
 }
